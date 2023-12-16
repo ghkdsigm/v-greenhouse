b5adc053f6902da47849345c019f4b90366503f9
ghkdsigm3 <ghkdsigm3@gmail.com>
2023-12-16 13:14:09 +0900

    작업중
    undefined

---
+++ components/common/formType02.vue
@@ -0,0 +1,65 @@
+<template>
+	<form action="" class="flex border-t border-gray-200 lg:border-t-0">
+		<fieldset class="w-full">
+			<legend class="block w-full bg-gray-50 px-5 py-3 text-xs font-medium">Type</legend>
+
+			<div class="space-y-2 px-5 py-6">
+				<div class="flex items-center">
+					<input id="New" type="checkbox" name="type[New]" class="h-5 w-5 rounded border-gray-300" checked />
+
+					<label for="New" class="ml-3 text-sm font-medium"> New </label>
+				</div>
+
+				<div class="flex items-center">
+					<input id="Used" type="checkbox" name="type[Used]" class="h-5 w-5 rounded border-gray-300" />
+
+					<label for="Used" class="ml-3 text-sm font-medium"> Used </label>
+				</div>
+
+				<div class="flex items-center">
+					<input id="Branded" type="checkbox" name="type[Branded]" class="h-5 w-5 rounded border-gray-300" />
+
+					<label for="Branded" class="ml-3 text-sm font-medium"> Branded </label>
+				</div>
+
+				<div class="pt-2">
+					<button type="button" class="text-xs text-gray-500 underline">Reset Type</button>
+				</div>
+			</div>
+		</fieldset>
+
+		<fieldset class="w-full">
+			<legend class="block w-full bg-gray-50 px-5 py-3 text-xs font-medium">Price</legend>
+
+			<div class="space-y-2 px-5 py-6">
+				<div class="flex items-center">
+					<input id="300+" type="radio" name="Price" value="300+" class="h-5 w-5 rounded border-gray-300" />
+
+					<label for="300+" class="ml-3 text-sm font-medium"> 300+ </label>
+				</div>
+
+				<div class="flex items-center">
+					<input id="600+" type="radio" name="Price" value="600+" class="h-5 w-5 rounded border-gray-300" />
+
+					<label for="600+" class="ml-3 text-sm font-medium"> 600+ </label>
+				</div>
+
+				<div class="flex items-center">
+					<input id="1500+" type="radio" name="Price" value="1500+" class="h-5 w-5 rounded border-gray-300" checked />
+
+					<label for="1500+" class="ml-3 text-sm font-medium"> 1500+ </label>
+				</div>
+
+				<div class="pt-2">
+					<button type="button" class="text-xs text-gray-500 underline">Reset Price</button>
+				</div>
+			</div>
+		</fieldset>
+	</form>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ components/common/selectType03.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="m-4">
+		<button
+			class="relative inline-flex cursor-pointer select-none items-center rounded-lg bg-blue-600 px-4 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-blue-300 hover:bg-blue-700"
+		>
+			<label for="dropdown-open" class="absolute left-0 block h-full w-full cursor-pointer"> </label>
+			Dropdown button
+			<svg
+				class="ml-2 h-4 w-4"
+				aria-hidden="true"
+				fill="none"
+				stroke="currentColor"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+			</svg>
+		</button>
+		<input id="dropdown-open" type="checkbox" class="peer hidden" checked />
+		<!-- Dropdown menu -->
+		<div class="peer-checked:block z-10 mt-2 hidden w-44 divide-y divide-gray-100 rounded border bg-white shadow">
+			<ul class="py-1 text-sm text-gray-700" aria-labelledby="dropdownDefault">
+				<li>
+					<a href="#" class="block px-4 py-2 hover:bg-gray-100">Dashboard</a>
+				</li>
+				<li>
+					<a href="#" class="block px-4 py-2 hover:bg-gray-100">Settings</a>
+				</li>
+				<li>
+					<a href="#" class="block px-4 py-2 hover:bg-gray-100">Support</a>
+				</li>
+			</ul>
+			<ul class="py-1 text-sm text-gray-700" aria-labelledby="dropdownDefault">
+				<li>
+					<a href="#" class="block px-4 py-2 hover:bg-gray-100">Sign out</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
